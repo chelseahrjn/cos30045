c0213865d54328046af795c750720bf8a7e5e7a9
Chelsea <87615817+chelseahrjn@users.noreply.github.com>
2024-06-07 20:32:40 +1000

    zoom in

--- chelsea/chelsea.js
+++ chelsea/chelsea.js
@@ -15,7 +15,6 @@ function init() {
 
         var select = d3.select("#yearSelector");
         // Populate the dropdown list
-
         select.selectAll("option")
             .data(years)
             .enter()
@@ -33,151 +32,8 @@ function init() {
             updateScatter(selectedYear);
         });
 
-        function updateScatter(selectedYear) {
-            // Filter data based on year
-            var filtered = data.filter(d => d.year === selectedYear);
-
-            // Convert the format
-            var dataset = [];
-
-            filtered.forEach(d => {
-                dataset.push({
-                    country: d.country,
-                    pharma: d.type === "PHARMA" ? d.value : null,
-                    deaths: d.type === "DEATH" ? d.value : null,
-                    year: d.year
-                });
-            });
-
-            // Combine data points of each country into a single object
-            var scatterData = dataset.reduce((acc, curr) => {
-                var item = acc.find(d => d.country === curr.country);
-
-                if (item) {
-                    item.pharma = curr.pharma || item.pharma;
-                    item.deaths = curr.deaths || item.deaths;
-                }
-                else {
-                    acc.push(curr);
-                }
-                return acc;
-            }, []);
-
-            console.log(scatterData);
-
-
-            // Update scatter plot with transitions
-            var svg = d3.select("#chelseaChart svg");
-            
-            // Check if svg exists
-            if (svg.empty()) {
-                scatterChelsea(dataset, "#chelseaChart");
-            }
-            else {
-                // Update scales of new data
-                var maxPharma = d3.max(scatterData, d => (d.pharma !== null) ? d.pharma : 0);
-                var maxDeaths = d3.max(scatterData, d => (d.deaths !== null) ? d.deaths : 0);
-    
-                var xScale = d3.scaleLinear()
-                    .domain([0, maxPharma])
-                    .range([padding, w - padding]);
-    
-                var yScale = d3.scaleLinear()
-                    .domain([0, maxDeaths])
-                    .range([h - padding, padding]);
-    
-                var xAxis = d3.axisBottom(xScale);
-                var yAxis = d3.axisLeft(yScale);
-    
-                svg.select(".x-axis")
-                    .transition()
-                    .duration(500)
-                    .call(xAxis);
-    
-                svg.select(".y-axis")
-                    .transition()
-                    .duration(500)
-                    .call(yAxis);
-    
-                // Bind the new data
-                var circles = svg.selectAll("circle")
-                    .data(scatterData, d => d.country);
-    
-                // Update the existing circles
-                circles.transition()
-                    .duration(500)
-                    .attr("cx", d => xScale(d.pharma))
-                    .attr("cy", d => yScale(d.deaths))
-                    .attr("r", 5)
-                    .attr("fill", "steelblue")
-                    .style("opacity", 0.8);
-    
-                // Enter the new circles
-                circles.enter()
-                    .append("circle")
-                    .attr("cx", d => xScale(d.pharma))
-                    .attr("cy", d => yScale(d.deaths))
-                    .attr("r", 5)
-                    .attr("fill", "steelblue")
-                    .style("opacity", 0.8)
-                    .on("mouseover", mouseover)
-                    .on("mousemove", mousemove)
-                    .on("mouseleave", mouseleave)
-                    .transition()
-                    .duration(500);
-                
-                // Remove the old circles
-                circles.exit()
-                    .transition()
-                    .duration(500)
-                    .attr("r", 0)
-                    .remove();
-            }
-
-
-            // // ????
-            // // Clear the previous scatter
-            // d3.select("#chelseaChart").html("");
-
-            // console.log(dataset);
-            // scatterChelsea(dataset, "#chelseaChart");
-        }
-    })
-        .catch(function (error) { // Display error message if any
-            console.log("Error loading data: " + error);
-        });
-
-    function scatterChelsea(data, chartId) {
-
-        var svg = d3.select(chartId)
-            .append("svg")
-            .attr("width", w)
-            .attr("height", h);
-
-        // Check for valid data before setting scale domains
-        var maxPharma = d3.max(data, d => (d.pharma !== null) ? d.pharma : 0);
-        var maxDeaths = d3.max(data, d => (d.deaths !== null) ? d.deaths : 0);
-
-        var xScale = d3.scaleLinear()
-            .domain([0, maxPharma])
-            .range([padding, w - padding]);
-
-        var yScale = d3.scaleLinear()
-            .domain([0, maxDeaths])
-            .range([h - padding, padding]);
-
-        // var xScale = d3.scaleLinear()
-        //     .domain([0, d3.max(data, d => d.pharma)])
-        //     .range([padding, w - padding]);
-
-        // var yScale = d3.scaleLinear()
-        //     .domain([0, d3.max(data, d => d.deaths)])
-        //     .range([h - padding, padding]);
-
-        var xAxis = d3.axisBottom(xScale);
-        var yAxis = d3.axisLeft(yScale);
-
-        var tooltip = d3.select(chartId)
+        // Event handlers for tooltip and mouse events
+        var tooltip = d3.select("#chelseaChart")
             .append("div")
             .attr("class", "tooltip")
             .style("opacity", 0)
@@ -188,64 +44,222 @@ function init() {
             .style("position", "absolute")
             .style("font-size", "10px");
 
-        var mouseover = function (d) {
+        function mouseover(event, d) {
             tooltip.style("opacity", 1);
             d3.select(this)
                 .style("stroke", "#212121")
-                .style("opacity", 1)
+                .style("opacity", 1);
         }
 
-        var mousemove = function (event, d) {
+        function mousemove(event, d) {
             tooltip.html(`${d.country} (${d.year}): <br> >> ${d.pharma} DDD per 1000 Inhabitants per Day <br> >> ${d.deaths} deaths per 1000 deaths`)
                 .style("left", (event.pageX + 10) + "px")
                 .style("top", (event.pageY - 28) + "px");
         }
 
-        var mouseleave = function (d) {
+        function mouseleave(event, d) {
             tooltip.style("opacity", 0);
             d3.select(this)
                 .style("stroke", "none")
                 .style("opacity", 0.8);
         }
 
-        svg.append("g")
-            .attr("transform", `translate(0, ${h - padding})`)
-            .call(xAxis);
+        function updateScatter(selectedYear) {
+            // Filter data based on year
+            var filtered = data.filter(d => d.year === selectedYear);
 
-        svg.append("g")
-            .attr("transform", `translate(${padding}, 0)`)
-            .call(yAxis);
+            // Convert the format
+            var dataset = filtered.map(d => ({
+                country: d.country,
+                pharma: d.type === "PHARMA" ? d.value : null,
+                deaths: d.type === "DEATH" ? d.value : null,
+                year: d.year
+            }));
 
-        // Add scatter points
-        svg.selectAll("circle")
-            .data(data.filter(d => d.pharma !== null && d.deaths !== null)) // Filter out null values
-            .enter()
-            .append("circle")
-            .attr("cx", d => xScale(d.pharma))
-            .attr("cy", d => yScale(d.deaths))
-            .attr("r", 5)
-            .attr("fill", "steelblue")
-            .style("opacity", 0.8)
-            .on("mouseover", mouseover)
-            .on("mousemove", mousemove)
-            .on("mouseleave", mouseleave);
+            // Combine data points of each country into a single object
+            var scatterData = dataset.reduce((acc, curr) => {
+                var item = acc.find(d => d.country === curr.country);
 
-        // X axis label
-        svg.append("text")
-            .attr("class", "x-axis-label")  // Class for styling
-            .attr("x", w / 2)  // Center the label
-            .attr("y", h - padding + 30)  // Position below the axis
-            .attr("text-anchor", "middle")
-            .text("Drugs Consumption in Defined Daily Dosage (DDD) per 1000 Inhabitants per Day");
+                if (item) {
+                    item.pharma = curr.pharma || item.pharma;
+                    item.deaths = curr.deaths || item.deaths;
+                } else {
+                    acc.push(curr);
+                }
+                return acc;
+            }, []);
 
-        // Y axis label
-        svg.append("text")
-            .attr("class", "y-axis-label")  // Class for styling
-            .attr("x", padding)
-            .attr("y", padding - 10)  // Position to left of the axis
-            .attr("text-anchor", "middle")
-            .text("Number of Deaths");
-    }
+            // Filter out data points with null values for pharma or deaths
+            scatterData = scatterData.filter(d => d.pharma !== null && d.deaths !== null);
+
+            // Update scatter plot with transitions
+            var svg = d3.select("#chelseaChart svg");
+
+            // Check if svg exists
+            if (!svg.empty()) {
+                // Update scales of new data
+                var maxPharma = d3.max(scatterData, d => d.pharma);
+                var maxDeaths = d3.max(scatterData, d => d.deaths);
+
+                var xScale = d3.scaleLinear()
+                    .domain([0, maxPharma])
+                    .range([padding, w - padding]);
+
+                var yScale = d3.scaleLinear()
+                    .domain([0, maxDeaths])
+                    .range([h - padding, padding]);
+
+                var xAxis = d3.axisBottom(xScale)
+                    .tickSize(-h + 2 * padding);
+                var yAxis = d3.axisLeft(yScale)
+                    .tickSize(-w + 2 * padding);
+
+                // Update x-axis and y-axis marks
+                svg.select(".x-axis")
+                    .transition()
+                    .duration(500)
+                    .call(xAxis);
+
+                svg.select(".y-axis")
+                    .transition()
+                    .duration(500)
+                    .call(yAxis);
+
+                // Bind the new data
+                var circles = svg.selectAll("circle")
+                    .data(scatterData, d => d.country);
+
+                // Update the existing circles
+                circles.transition()
+                    .duration(500)
+                    .attr("cx", d => xScale(d.pharma))
+                    .attr("cy", d => yScale(d.deaths))
+                    .attr("r", 5);
+
+                // Enter the new circles
+                circles.enter()
+                    .append("circle")
+                    .attr("cx", d => xScale(d.pharma))
+                    .attr("cy", d => yScale(d.deaths))
+                    .attr("r", 0)
+                    .style("opacity", 0.8)
+                    .attr("fill", "steelblue")
+                    .on("mouseover", mouseover)
+                    .on("mousemove", mousemove)
+                    .on("mouseleave", mouseleave)
+                    .transition()
+                    .duration(500)
+                    .attr("r", 5); // Transition to expand circle
+
+                // Remove the old circles
+                circles.exit()
+                    .transition()
+                    .duration(500)
+                    .attr("r", 0)
+                    .remove();
+
+                svg.call(zoomInstance);
+            }
+            else {
+
+                scatterChelsea(scatterData, "#chelseaChart");
+            }
+        }
+
+        var zoomInstance;
+
+        function scatterChelsea(data, chartId) {
+            var svg = d3.select(chartId)
+                .append("svg")
+                .attr("width", w)
+                .attr("height", h);
+
+            // Check for valid data before setting scale domains
+            var maxPharma = d3.max(data, d => d.pharma);
+            var maxDeaths = d3.max(data, d => d.deaths);
+
+            var xScale = d3.scaleLinear()
+                .domain([0, maxPharma])
+                .range([padding, w - padding]);
+
+            var yScale = d3.scaleLinear()
+                .domain([0, maxDeaths])
+                .range([h - padding, padding]);
+
+            var xAxis = d3.axisBottom(xScale)
+                .tickSize(-h + 2 * padding);
+            var yAxis = d3.axisLeft(yScale)
+                .tickSize(-w + 2 * padding);
+
+            // Append x and y axes
+            var gX = svg.append("g")
+                .attr("transform", `translate(0, ${h - padding})`)
+                .attr("class", "x-axis")
+                .call(xAxis);
+
+            var gY = svg.append("g")
+                .attr("transform", `translate(${padding}, 0)`)
+                .attr("class", "y-axis")
+                .call(yAxis);
+
+            // Add scatter points
+            svg.selectAll("circle")
+                .data(data.filter(d => d.pharma !== null && d.deaths !== null)) // Filter out null values
+                .enter()
+                .append("circle")
+                .attr("cx", d => xScale(d.pharma))
+                .attr("cy", d => yScale(d.deaths))
+                .attr("r", 5)
+                .attr("fill", "steelblue")
+                .style("opacity", 0.8)
+                .on("mouseover", mouseover)
+                .on("mousemove", mousemove)
+                .on("mouseleave", mouseleave);
+
+            // X axis label
+            svg.append("text")
+                .attr("class", "x-axis-label")  // Class for styling
+                .attr("x", w / 2)  // Center the label
+                .attr("y", h - padding + 30)  // Position below the axis
+                .attr("text-anchor", "middle")
+                .text("Drugs Consumption in Defined Daily Dosage (DDD) per 1000 Inhabitants per Day");
+
+            // Y axis label
+            svg.append("text")
+                .attr("class", "y-axis-label")  // Class for styling
+                .attr("x", padding)
+                .attr("y", padding - 10)  // Position to left of the axis
+                .attr("text-anchor", "middle")
+                .text("Number of Deaths");
+
+            // Zoom feature
+            zoomInstance = d3.zoom()
+                .scaleExtent([0.5, 4])
+                .on("zoom", function (event) {
+                    var transform = event.transform;
+
+                    // Apply zoom to circles
+                    svg.selectAll("circle")
+                        .attr("cx", d => transform.rescaleX(xScale)(d.pharma))
+                        .attr("cy", d => transform.rescaleY(yScale)(d.deaths));
+
+                    // Update axes
+                    gX.call(xAxis.scale(transform.rescaleX(xScale)));
+                    gY.call(yAxis.scale(transform.rescaleY(yScale)));
+                });
+
+            svg.call(zoomInstance);
+
+            d3.select(".reset")
+                .on("click", function () {
+                    zoomInstance.transform(svg, d3.zoomIdentity);
+                });
+        }
+
+    }).catch(function (error) {
+        console.log("Error loading data: " + error);
+    });
+
 }
 
 window.onload = init;
